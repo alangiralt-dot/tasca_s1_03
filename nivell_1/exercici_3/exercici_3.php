5e2cfaa7256b0fb575f6275412e566157dbc2837
Alan GIRALT <alangiralt@telefonica.net>
2026-04-16 13:57:58 +0200

    fix: use correct parameter and optimize case-insensitivity in hasCommonChar

--- nivell_1/exercici_3/exercici_3.php
+++ nivell_1/exercici_3/exercici_3.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types=1);
 $ictWords = ["server", "network", "device", "kernel", "router"];
-function hasCommonChar(array $words, string $char): bool {
-    foreach ($ictWords as $w) {
-        if (!str_contains(strtoupper($w), $char)) return false;
+function hasCommonChar(array $strings, string $char): bool {
+    $uppercaseChar = strtoupper($char);
+    foreach ($strings as $s) {
+        if (!str_contains(strtoupper($s), $uppercaseChar)) return false;
     }
     return true;
 }
+var_dump(hasCommonChar($ictWords, 'e'));
