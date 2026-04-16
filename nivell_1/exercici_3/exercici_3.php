5229bc32346cc1522d74c991c466d6c9287d2e98
Alan GIRALT <alangiralt@telefonica.net>
2026-04-16 18:45:25 +0200

    feat: add fail-fast to hasCommonChar function

--- nivell_1/exercici_3/exercici_3.php
+++ nivell_1/exercici_3/exercici_3.php
@@ -1,7 +1,8 @@
 <?php
 declare(strict_types=1);
 $ictWords = ["server", "network", "device", "kernel", "router"];
-function hasCommonChar(array $strings, string $char): bool {
+function hasCommonChar(array $strings, string $char): ?bool {
+    if (count($strings) === 0 || $char === '') return null;
     $uppercaseChar = strtoupper($char);
     foreach ($strings as $s) {
         if (!str_contains(strtoupper($s), $uppercaseChar)) return false;
